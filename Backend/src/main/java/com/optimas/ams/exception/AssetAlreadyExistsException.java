2ed30c097e1ab0acda83bfc2a8d8c8074ffdd28a
akash0155182 <as0155182@gmail.com>
2025-03-14 10:43:53 +0530

    initial commit

--- Backend/src/main/java/com/optimas/ams/exception/AssetAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.optimas.ams.exception;
-
-import jakarta.ws.rs.core.Response;
-
-public class AssetAlreadyExistsException extends CustomException {
-    public AssetAlreadyExistsException(String message) {
-        super(message, Response.Status.CONFLICT); // HTTP 409 Conflict
-    }
-}
--- Backend/src/main/java/com/optimas/ams/exception/DatabaseException.java
+++
@@ -1,9 +0,0 @@
-package com.optimas.ams.exception;
-
-import jakarta.ws.rs.core.Response;
-
-public class DatabaseException extends CustomException {
-    public DatabaseException(String message) {
-        super(message, Response.Status.INTERNAL_SERVER_ERROR); // HTTP 500
-    }
-}
